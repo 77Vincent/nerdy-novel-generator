e08988e9fb7615ac0ab3ed952515720aec9579e2
77Vincent <wentianqi77@outlook.com>
2024-09-13 14:27:51 +0900

    wip

--- src/generate.js
+++ src/generate.js
@@ -3,42 +3,53 @@ import {BASE_PROMPT, genre, pick, randInt} from "./utils.js";
 
 export const generate = async () => {
     const apiKey = process.env.OPEN_AI_API_KEY;
-
     const openai = new OpenAI({
         apiKey,
     });
 
-    const prompt0 = `${BASE_PROMPT}想一个题材为${pick(genre, randInt(1,3)).join(",")}的故事，${randInt(10,40)}字。`
-
     try {
-        const overview = await openai.chat.completions.create({
+        const promptForOverview = `${BASE_PROMPT}想一个题材为${pick(genre, randInt(1,3)).join(",")}的故事，${randInt(10,40)}字。`
+        const synopsis = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
                 {
                     role: "user",
-                    content: prompt0,
+                    content: promptForOverview,
                 },
             ],
             n: 1,
         });
 
-        const len = randInt(50, 100)
-        const prompt1 = `${BASE_PROMPT}以下是故事概要：${overview.choices[0].message.content}写一个充满悬念的小说或故事，别重复概要的内容，${len}字，不要太正经，没意思`;
+        const promptForTitle = `${BASE_PROMPT}以下是故事概要：${synopsis.choices[0].message.content}为它想一个原创性的名字，${randInt(2,15)}字以内，只需要名字`
+        const title = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [
+                {
+                    role: "user",
+                    content: promptForTitle,
+                },
+            ],
+            n: 1,
+        });
+
+        const len = randInt(10, 30)
+        const promptForContent = `${BASE_PROMPT}以下是故事概要：${synopsis.choices[0].message.content}写一个充满悬念的小说或故事，别重复概要的内容，${len}字，不要太正经，没意思`;
 
         const content = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
                 {
                     role: "user",
-                    content: prompt1,
+                    content: promptForContent,
                 },
             ],
             n: 1,
         });
 
         return {
-            title: "",
-            overview: overview.choices[0].message.content,
+            title: title.choices[0].message.content,
+            length: len,
+            synopsis: synopsis.choices[0].message.content,
             story: content.choices[0].message.content,
         };
     } catch (e) {
