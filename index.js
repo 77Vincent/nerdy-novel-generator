5dfe3b2f76bb73ed19562573cd73afe5907b7508
77Vincent <wentianqi77@outlook.com>
2024-09-30 17:44:54 +0900

    author generation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {generate} from "./src/generate.js";
 import {putNovel} from "./src/db.js";
 import {uploadImage} from "./src/s3.js";
+import {uploadAuthor} from "./src/author.js";
 
 (async function () {
     const novel = await generate('micro')
     await putNovel(novel)
     await uploadImage(novel.fileName)
+    await uploadAuthor(String(novel.author_id))
 }())
